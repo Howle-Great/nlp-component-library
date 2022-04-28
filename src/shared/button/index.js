365b504b7d8067080bfe3079ed936feb1265a354
Дмитрий <mdv413@mail.ru>
2022-04-28 22:19:59 +0300

    Add simple export

---
+++ src/shared/button/index.js
@@ -0,0 +1,7 @@
+const Button = () => <button style="background-color: yellow;">Button</button>;
+const SupperButton = () => (
+  <button style="background-color: yellow; widht: 240px; height: 48px;">
+    Button
+  </button>
+);
+export { Button, SupperButton };
